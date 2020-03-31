3632377eaa48e63a60b0c8131a605266338317f4
Ya_Myn <ricky.month@gmail.com>
2020-03-31 19:49:02 +0300

    Updates

---
+++ precache-manifest.b85f3b712ed957fdf389f639ce2fa849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f349e1454af20308b2f221854e2d814",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "e88f2e045c07150744b4",
+    "url": "/goit-react-hw-01-components/static/css/main.3bfc2163.chunk.css"
+  },
+  {
+    "revision": "de860ca52efee61df7e1",
+    "url": "/goit-react-hw-01-components/static/js/2.e2bde79c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.e2bde79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88f2e045c07150744b4",
+    "url": "/goit-react-hw-01-components/static/js/main.71850a7f.chunk.js"
+  },
+  {
+    "revision": "f41507ba7c9aee7f0098",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.0100adac.js"
+  }
+]);
